3d7b6b3bc81b7c084be29ece8b74474954ff184f
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-11-07 19:26:27 +0800

    Modify Register page & css

--- Frontend/nalc/src/Screen/Register/Register.js
+++ Frontend/nalc/src/Screen/Register/Register.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import nalcLogo from '../../nalcLogo.png';
+import './Register.css'
 
 function Register() {
   const [email, setEmail] = useState('');
@@ -13,37 +15,41 @@ function Register() {
   };
 
   return (
-    <div>
-      <h2>Register</h2>
-      <form>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <div className='containter-fluid'>
+      <div className='big-space'></div>
+      <div className='row logo-r'>
+        <img src={nalcLogo} alt="NALC Logo" />
+      </div>
+      <div className='title-r'>
+        <div className='row'>
+          <h2 className='text-maroon'><strong>Create Account</strong></h2>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
-        <button type="button" onClick={handleRegister}>
-          Register
-        </button>
-      </form>
+      </div>
+      <div className='row inputs'>
+        <form>
+            <div className='inputField' style={{width: 370, height: 48, position: 'relative'}}>
+                {/* <div style={{width: 370, height: 48, left: 0, top: 0, position: 'absolute', background: '#F6F6F6'}} /> */}
+                <input type="email" class="form-control" id="floatingInput" placeholder="Email"/>
+                <div style={{width: 6, height: 35, left: 0, top: 0, position: 'absolute', background: '#841818'}} />
+            </div>
+            <div className='inputField' style={{width: 370, height: 48, position: 'relative'}}>
+                {/* <div style={{width: 370, height: 48, left: 0, top: 0, position: 'absolute', background: '#F6F6F6'}} /> */}
+                <input type="password" class="form-control" id="floatingInput" placeholder="Password"/>
+                <div style={{width: 6, height: 35, left: 0, top: 0, position: 'absolute', background: '#841818'}} />
+            </div>
+            <div className='inputField' style={{width: 370, height: 48, position: 'relative'}}>
+                {/* <div style={{width: 370, height: 48, left: 0, top: 0, position: 'absolute', background: '#F6F6F6'}} /> */}
+                <input type="password" class="form-control" id="floatingInput" placeholder="Re-enter Password"/>
+                <div style={{width: 6, height: 35, left: 0, top: 0, position: 'absolute', background: '#841818'}} />
+            </div>
+            <br/>
+            <p className='haveAcc text-maroon'><strong> Already a member? </strong></p>
+            <br/>
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <button class="btn-register" type="button">Register</button>
+            </div>
+        </form>
+      </div>
     </div>
   );
 }
