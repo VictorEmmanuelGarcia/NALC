4f2324260fbaeb2c9bc840c3db83254bcc32905d
John William Miones <johnwilliam.miones@cit.edu>
2023-11-09 23:29:27 +0800

    Add url for admin

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views  # Import your views module
-from .views import ThreadListCreateView, ThreadDetailView, MessageCreateView, MessageListView, ThreadListView
+from .views import ThreadListCreateView, ThreadDetailView, MessageCreateView, MessageListView, ThreadListView, upload_and_replace_data
 
 urlpatterns = [
     # Other URL patterns
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/messages/create/', MessageCreateView.as_view(), name='message-create'),
     path('api/messages/thread/<int:thread_id>/', MessageListView.as_view(), name='message-list'),
     path('api/threads/', ThreadListView.as_view(), name='thread-list'),
+    path('upload-and-replace-data/', views.upload_and_replace_data, name='upload-and-replace-data'),
 ]
