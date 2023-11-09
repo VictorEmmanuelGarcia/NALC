95dadc565f9ea830db3dbf91674da2ab9397ac98
John William Miones <johnwilliam.miones@cit.edu>
2023-11-09 22:43:44 +0800

    add url for get all threads

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views  # Import your views module
-from .views import ThreadListCreateView, ThreadDetailView, MessageCreateView, MessageListView
+from .views import ThreadListCreateView, ThreadDetailView, MessageCreateView, MessageListView, ThreadListView
 
 urlpatterns = [
     # Other URL patterns
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api/threads/<int:pk>/', ThreadDetailView.as_view(), name='thread-detail'),
     path('api/messages/create/', MessageCreateView.as_view(), name='message-create'),
     path('api/messages/thread/<int:thread_id>/', MessageListView.as_view(), name='message-list'),
+    path('api/threads/', ThreadListView.as_view(), name='thread-list'),
 ]
