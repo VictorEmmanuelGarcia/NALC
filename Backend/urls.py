6fce4c91f2cf8874d8363104b46f5d186942de76
John William Miones <johnwilliam.miones@cit.edu>
2023-11-15 11:11:41 +0800

    Add urls for remove all

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views  # Import your views module
-from .views import ThreadListCreateView, ThreadDetailView, UserThreadListView, MessageCreateView, MessageListView, upload_and_replace_data, UserRegisterView, UserLoginView, UserUpdateView, UserDetailsView
+from .views import ThreadListCreateView, ThreadDetailView, UserThreadListView, MessageCreateView, MessageListView, upload_and_replace_data, UserRegisterView, UserLoginView, UserUpdateView, UserDetailsView, DeleteAllThreads
 
 urlpatterns = [
     # Other URL patterns
     path('api/threads/', ThreadListCreateView.as_view(), name='thread-list'),
     path('api/threads/<int:pk>/', ThreadDetailView.as_view(), name='thread-detail'),
     path('api/messages/create/', MessageCreateView.as_view(), name='message-create'),
+    path('api/messages/delete-all/', DeleteAllThreads.as_view(), name='delete-all'),
     path('api/messages/thread/<int:thread_id>/', MessageListView.as_view(), name='message-list'),
     path('api/users/threads/', UserThreadListView.as_view(), name='thread-list'),
     path('upload-and-replace-data/', views.upload_and_replace_data, name='upload-and-replace-data'),
