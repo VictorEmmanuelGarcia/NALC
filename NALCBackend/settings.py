d16aca6ff31c8897216541626aa31e51c9da4132
John William Miones <johnwilliam.miones@cit.edu>
2023-12-20 12:51:33 +0800

    modify database configuration

--- NALCBackend/settings.py
+++ NALCBackend/settings.py
@@ -106,18 +106,20 @@ WSGI_APPLICATION = 'NALCBackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nalc',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'NAME': 'nalc-schema',
+        'USER': 'nalc',
+        'PASSWORD': '!p4msCitU',
+        'HOST': 'nalc.mysql.database.azure.com',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES",
             'charset': 'utf8mb4',
+            'ssl': {
+                'ca': '"C:\\Test\\DigiCertGlobalRootCA.crt.pem"',  # Adjust the path to your SSL certificate
+            },
         },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
